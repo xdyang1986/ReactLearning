0857e8013f723b377ec2ca8bf64691b1d22f8a54
Gordon Yang <xiaya@microsoft.com>
2017-05-07 23:18:15 +0800

    change react render to react dom

--- hn/js/NewsItemTest.js
+++ hn/js/NewsItemTest.js
@@ -1,6 +1,7 @@
 var $ = require('jquery');
 var NewsItem = require('./NewsItem');
 var React = require('react');
+var ReactDom = require('react-dom')
 
 $.ajax({
   url: '/json/items.json'
@@ -8,5 +9,5 @@ $.ajax({
   // Log the data so we can inspect it in the developer console.
   console.log('items', items);
   // Use a fake rank for now.
-  React.render(<NewsItem item={items[0]} rank={1}/>, $('#content')[0]);
+  ReactDom.render(<NewsItem item={items[0]} rank={1}/>, $('#content')[0]);
 });
